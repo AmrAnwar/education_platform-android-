6754b85c4a52e5ad772486f7fa6269562ed7a587
AhmedNader65 <ahmed.nader1994@gmail.com>
2017-07-23 19:56:40 +0200

    first push

---
+++ app/src/main/java/com/mrerror/tm/MainActivity.java
@@ -0,0 +1,48 @@
+package com.mrerror.tm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_news:
+                    mTextMessage.setText(R.string.title_news);
+                    return true;
+                case R.id.navigation_words:
+                    mTextMessage.setText(R.string.title_words);
+                    return true;
+                case R.id.inbox:
+                    mTextMessage.setText(R.string.title_inbox);
+                    return true;
+                case R.id.model_answer:
+                    mTextMessage.setText(R.string.title_answer);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
