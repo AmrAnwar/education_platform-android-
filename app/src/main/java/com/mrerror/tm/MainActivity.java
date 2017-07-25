3ca6570f697c266ea5adaeac7049f48f8f91a94f
kareem <kareemhamdy@gimal.com>
2017-07-25 16:58:22 +0200

    Local Commit kareem

--- app/src/main/java/com/mrerror/tm/MainActivity.java
+++ app/src/main/java/com/mrerror/tm/MainActivity.java
@@ -1,27 +1,45 @@
 package com.mrerror.tm;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.webkit.MimeTypeMap;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mrerror.tm.dataBases.Contract;
+import com.mrerror.tm.dataBases.ModelAnswerDbHelper;
 import com.mrerror.tm.fragments.GeneralNews;
 import com.mrerror.tm.fragments.GeneralWords;
+import com.mrerror.tm.fragments.ModelAnswerFragment;
 import com.mrerror.tm.fragments.PartsFragment;
 import com.mrerror.tm.fragments.UnitFragment;
 import com.mrerror.tm.fragments.WordsFragment;
+import com.mrerror.tm.models.ModelAnswer;
 import com.mrerror.tm.models.Unit;
 import com.mrerror.tm.models.Word;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements UnitFragment.OnListFragmentInteractionListener ,
-PartsFragment.OnListFragmentInteractionListener{
+PartsFragment.OnListFragmentInteractionListener ,ModelAnswerFragment.OnItemClick{
 
     private TextView mTextMessage;
-
+    DownloadManager downloadManager;
+    long reference;
+    ModelAnswerDbHelper dpHelper;
+    ModelAnswer mModelAnswer;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -38,6 +56,7 @@ PartsFragment.OnListFragmentInteractionListener{
 //                    mTextMessage.setText(R.string.title_inbox);
 //                    return true;
                 case R.id.model_answer:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content,new ModelAnswerFragment()).commit();
                     return true;
             }
             return false;
@@ -52,7 +71,66 @@ PartsFragment.OnListFragmentInteractionListener{
         getSupportFragmentManager().beginTransaction().replace(R.id.content,new GeneralNews()).commit();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+         dpHelper=new ModelAnswerDbHelper(this);
+        BroadcastReceiver receiver =new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action=intent.getAction();
+                if(DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action))
+                {
+                    DownloadManager.Query req_query= new DownloadManager.Query();
+                    req_query.setFilterById(reference);
+                    Cursor c= downloadManager.query(req_query);
+
+                    if(c.moveToFirst()){
+
+                        int coulmIndex=c.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                        if (DownloadManager.STATUS_SUCCESSFUL==c.getInt(coulmIndex)){
+                            String uriString= c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+
+                            saveToDataBase(uriString,mModelAnswer);
+
+
+                        }
+                    }
+
+                }
+            }
+        };
+        registerReceiver(receiver,new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
     }
+
+
+    public  void downLoad(ModelAnswer modelAnswer ){
+
+        downloadManager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri= Uri.parse(modelAnswer.getFileUrl());
+        DownloadManager.Request request= new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+         reference= downloadManager.enqueue(request);
+
+
+    }
+    public  void saveToDataBase(String uri,ModelAnswer modelAnswer){
+
+        Toast.makeText(this, uri+ " "+ modelAnswer.getId(), Toast.LENGTH_SHORT).show();
+        SQLiteDatabase db = dpHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Contract.TableForModelAnswer._ID, modelAnswer.getId());
+        values.put(Contract.TableForModelAnswer.COLUMN_FILE_PATH,uri.toString());
+
+
+      long check=  db.insert(Contract.TableForModelAnswer.TABLE_NAME,null,values);
+
+        if(check>=0)
+            Toast.makeText(this, "Done add to dataBase", Toast.LENGTH_SHORT).show();
+               else
+            Toast.makeText(this, "errorHappend", Toast.LENGTH_SHORT).show();
+
+    }
+
 
     // For unit list
     @Override
@@ -65,5 +143,35 @@ PartsFragment.OnListFragmentInteractionListener{
     @Override
     public void onListFragmentInteraction(ArrayList<Word> item) {
         getSupportFragmentManager().beginTransaction().replace(R.id.content, WordsFragment.newInstance(item)).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void onItemClickLestiner(ModelAnswer item) {
+        Toast.makeText(this, item.getNote(), Toast.LENGTH_SHORT).show();
+        mModelAnswer=item;
+
+     if(!mModelAnswer.getDwonload())
+     {
+         downLoad(item);}
+     else {
+
+         Intent i=new Intent(MainActivity.this,ReadPDFactivity.class);
+         i.putExtra("file_path",mModelAnswer.getFilePath());
+         startActivity(i);
+//         Toast.makeText(this, getMimeType(item.getFilePath()), Toast.LENGTH_SHORT).show();
+     }
+
+    }
+    public static String getMimeType(String url) {
+        String type = null;
+        String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        }
+        return type;
+    }
+    protected void onDestroy() {
+        dpHelper.close();
+        super.onDestroy();
     }
 }
