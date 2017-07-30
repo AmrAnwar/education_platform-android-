0d7cd0eabadb8b0a9ee2ed1b788b1458a37baaba
kareem <kareemhamdy@gimal.com>
2017-07-30 09:20:54 +0200

    done progressBar  may need some changes

--- app/src/main/java/com/mrerror/tm/ReplyForStaffActivity.java
+++ app/src/main/java/com/mrerror/tm/ReplyForStaffActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
